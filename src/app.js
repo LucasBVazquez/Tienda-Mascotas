1d5c6cfce4b2b234185792d95b07d3bf74a61538
Lucas450B <lucasvazquez828@gmail.com>
2023-08-19 23:23:17 -0300

    Se agrego imagenes y el heder del html

--- src/app.js
+++ src/app.js
@@ -4,14 +4,16 @@ const path = require("path");
 const app = express();
 
 
-
+//usando los recursos
 app.use(express.static("public"))
 app.set('views', path.join(__dirname, 'views'));
 
-
+//poniendo a escuchar al servidor
 app.listen(3000, () => console.log(`Servidor corriendo en http://localhost:3000`));
 
+
+//todas las rutas get
 app.get("/", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/home.html");
+    let htmlPath = path.resolve(__dirname, "./views/index.html");
     res.sendFile(htmlPath)
 });
